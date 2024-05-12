946a547d40551f4e65adefff92f77552252c02ae
tanwb <tan_waibin@163.com>
2024-05-12 17:33:33 +0800

    feat: add i18n

--- src/components/demoComp/index.tsx
+++ src/components/demoComp/index.tsx
@@ -1,10 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "assets/react.svg";
 import viteLogo from "/vite.svg";
+import { useTranslation } from "react-i18next";
 import "./style.css";
 
 function DemoComp() {
+  const { t, i18n } = useTranslation();
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    i18n.changeLanguage("en"); // 改变语言，同步国际化
+  }, [i18n]);
 
   return (
     <>
@@ -27,6 +33,9 @@ function DemoComp() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
+      </p>
+      <p>
+        <span>翻译示例：{t("common.more")}</span>
       </p>
     </>
   );
