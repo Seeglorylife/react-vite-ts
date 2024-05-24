3ec4ff8c5b800fb21ef9182dce5f19cd97a13012
tanwb <tan_waibin@163.com>
2024-05-24 10:29:47 +0800

    feat: add unocss error shims

---
+++ src/shims.d.ts
@@ -0,0 +1,27 @@
+/** unocss前缀使用报错处声明 **/
+import * as React from 'react';
+declare module 'react' {
+	interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
+		flex?: boolean;
+		relative?: boolean;
+		text?: string;
+		grid?: boolean;
+		before?: string;
+		after?: string;
+		shadow?: boolean;
+		w?: string;
+		h?: string;
+		bg?: string;
+		rounded?: string;
+		fixed?: boolean;
+		b?: string;
+		z?: string;
+		block?: boolean;
+		'focus:shadow'?: boolean;
+		border?: string;
+	}
+	interface SVGProps<T> extends SVGAttributes<T>, ClassAttributes<T> {
+		w?: string;
+		h?: string;
+	}
+}
